b31bafcb4a055c952d687428437dfa537874091c
Paul-Antoine <50651783+pamtrgl@users.noreply.github.com>
2021-04-12 09:21:02 +0200

    Revision 0.2
    The revision include logical output to check behavioral aspect.

--- FeFET.vhd
+++ FeFET.vhd
@@ -14,8 +14,8 @@
 -- Dependencies: 
 -- 
 -- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
+-- Revision 0.2 - 12/04/21
+-- Additional Comments: This revision add a logical port to check behavioral aspect.
 ----------------------------------------------------------------------------------
 
 
@@ -35,41 +35,59 @@ library ieee_proposed;
 use ieee_proposed.fixed_float_types.all;
 use ieee_proposed.fixed_pkg.all;
 
-entity FeFET1 is 
-    Port (  Vi : in std_logic_vector(23 downto 0); -- 24 bits precision
-            Vo : out std_logic_vector(23 downto 0)); -- 24 bits precision
+entity FeFET is 
+    Port (  Vi      : in std_logic_vector(23 downto 0);         -- 24 bits precision
+            Vo      : out std_logic_vector(23 downto 0);       -- 24 bits precision
+            Qo      : out std_logic:= '0' );                    -- logic output
+                
             
-end FeFET1;
+end FeFET;
 
-architecture Behavioral of FeFET1 is 
-    signal A: std_logic := '0';
-    signal ViR: sfixed(8 downto -15):= X"000000"; -- 24 bits precision
-    signal VoR: sfixed(8 downto -15); -- 24 bits precision
+architecture Behavioral of FeFET is 
+    signal A: std_logic := '0';                     -- in bit (pulse)
+    signal B: std_logic := '0';                     -- stored bit
+    signal P: std_logic := '0';                     -- polarization signal (0=HIGH; 1=LOW)
+    signal ViR: sfixed(8 downto -15):= X"000000";   -- 24 bits precision
+    signal VoR: sfixed(8 downto -15):= X"000000";   -- 24 bits precision
+    
+    --constant Rmin: sfixed(8 downto -15):= X"008000";    -- Rmin = 1ohm
+    --constant Rmax: sfixed(8 downto -15):= X"050000";    -- Rmax = 10ohm
+    
 
 begin 
     
     ViR <= resize(to_sfixed(Vi, 8, -15) * to_sfixed(0.015625, 8, -15), 8, -15); --Conversion of Vi to real value --ViR E [-4; +4]
     Vo <= to_slv(resize(VoR, 8, -15));
+    Qo <= not(A and B);
     
-    process(ViR, VoR, A)
+    pls: process(ViR)     -- pulse
+    begin
+        if(ViR >= to_sfixed(1.2, ViR)) then
+            A <= '1';
+        else
+            A <= '0';
+        end if;
+    end process pls;
+    
+    pol: process(ViR, VoR, P)
     begin
     --Updating polarization
     --If ViR E [-2; 2], Vo is arbitrary
         if(ViR < to_sfixed(-2.0, ViR)) then 
             if (ViR < to_sfixed(-2.5, ViR))  then 
-                A <= '0';
-                
+                P <= '0'; -- high polarity
+                B <= '1';
             end if;
             VoR <= to_sfixed(1.2, VoR);
             
         elsif(ViR > to_sfixed(2.0, ViR))  then 
             if (ViR > to_sfixed(2.5, ViR))  then 
-                A <= '1';
-                
+                P <= '1'; -- low polarity
+                B <= '0';
             end if;
             VoR <= to_sfixed(0.03, VoR);
             
-        elsif(A = '0') then
+        elsif(P = '0') then
             ---- -4V polarization 
             if (ViR < to_sfixed(0.0, ViR)) then
             --VoR <= 1.2;
@@ -98,10 +116,9 @@ begin
             --VoR <= -0.014*(ViR-1.42)+0.04;
                 VoR <= resize(to_sfixed(-0.014, 8, -15) * (ViR - to_sfixed(1.42, 8, -15))
                  + to_sfixed(0.04, 8, -15), 8, -15);
-                 
             end if;
         
-        elsif(A = '1') then
+        elsif(P = '1') then
             ---- +4V polarization 
             if(ViR < to_sfixed(-0.7, ViR)) then
             --VoR <= 1.17-0.1*(ViR+0.1)-0.1*(ViR+0.1)**2;
@@ -124,9 +141,9 @@ begin
             --VoR <= 0.03-8.0*(ViR-2.0);
                 VoR <= resize(to_sfixed(0.03, 16, -46)
                 - to_sfixed(8.0, 8, -15) * (ViR - to_sfixed(2.0, 8, -15)), 8, -15);
-                        
+                
             end if;            
         end if;
-    end process;
+    end process pol;
     
 end Behavioral;
